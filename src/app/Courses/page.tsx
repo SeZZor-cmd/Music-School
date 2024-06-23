84a5b2dca4e1a72c12dde3bf9ce41966f065e0eb
SeZZor-cmd <das.akash0504@gmail.com>
2024-06-23 21:25:28 +0530

    sec commit

--- src/app/Courses/page.tsx
+++ src/app/Courses/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import Link from "next/link";
 import courseData from "@/data/music_courses.json"
-export default function page() {
+export default function Page() {
   return (
     <div className="min-h-screen bg-black py-12 pt-36">
       <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All Courses {courseData.courses.length}</h1>
